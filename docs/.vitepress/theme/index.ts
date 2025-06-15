ff9ad8e252aa230ac1c8dd4b860dc903ac10e9a1
小钻风 <7418371+xiaozuanfeng@user.noreply.gitee.com>
2025-06-15 18:38:30 +0800

    add files

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,21 @@
+// https://vitepress.dev/guide/custom-theme
+// @ts-ignore
+import { h } from 'vue'
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import './style.css'
+
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  }
+} satisfies Theme
